a720fb832fa5d3ceaa02aeb1ac5a7711a992f0f6
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-05-11 14:59:56 -0400

    fixing errors

--- Thursday/traceability/server.js
+++ Thursday/traceability/server.js
@@ -6,4 +6,14 @@ const cors = require(`cors`);
 
 app.use(express.static(`${__dirname}/public`))
 
+
+const { ROLLBAR_ACCESS_TOKEN } = process.env;
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.listen(4000, () => console.log(`Running on server 4000.`));
